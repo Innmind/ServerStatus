e29a3f112a24e72f25dc85091b7952951d72e690
Baptiste Langlade <baptiste.langlade@hey.com>
2025-05-08 10:00:48 +0200

    use blackbox to generate points in time

--- tests/Server/ProcessTest.php
+++ tests/Server/ProcessTest.php
@@ -11,28 +11,35 @@ use Innmind\Server\Status\Server\{
     Process\Memory,
     Cpu\Percentage,
 };
-use Innmind\TimeContinuum\PointInTime;
 use Innmind\Immutable\Maybe;
+use Fixtures\Innmind\TimeContinuum\Earth\PointInTime;
+use Innmind\BlackBox\PHPUnit\BlackBox;
 use PHPUnit\Framework\TestCase;
 
 class ProcessTest extends TestCase
 {
+    use BlackBox;
+
     public function testInterface()
     {
-        $process = new Process(
-            $pid = new Pid(1),
-            $user = new User('root'),
-            $cpu = new Percentage(42),
-            $memory = new Memory(42),
-            $start = Maybe::just($this->createMock(PointInTime::class)),
-            $command = new Command('/sbin/launchd'),
-        );
+        $this
+            ->forAll(PointInTime::any())
+            ->then(function($pointInTime) {
+                $process = new Process(
+                    $pid = new Pid(1),
+                    $user = new User('root'),
+                    $cpu = new Percentage(42),
+                    $memory = new Memory(42),
+                    $start = Maybe::just($pointInTime),
+                    $command = new Command('/sbin/launchd'),
+                );
 
-        $this->assertSame($pid, $process->pid());
-        $this->assertSame($user, $process->user());
-        $this->assertSame($cpu, $process->cpu());
-        $this->assertSame($memory, $process->memory());
-        $this->assertSame($start, $process->start());
-        $this->assertSame($command, $process->command());
+                $this->assertSame($pid, $process->pid());
+                $this->assertSame($user, $process->user());
+                $this->assertSame($cpu, $process->cpu());
+                $this->assertSame($memory, $process->memory());
+                $this->assertSame($start, $process->start());
+                $this->assertSame($command, $process->command());
+            });
     }
 }
